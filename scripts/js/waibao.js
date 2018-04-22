196cbda49ba1a258fd913027701daf2aeca153bd
Donglin Pu <p.donglin@gmail.com>
2018-04-21 18:07:20 -0700

    draft done

--- scripts/js/waibao.js
+++ scripts/js/waibao.js
@@ -9,5 +9,25 @@ $(document).ready(function() {
 	   		'tooltips': ['Page 1', 'Page 2', 'Page 3', 'Page 4']
 	   	}
 	});
-
+	
+	$('#carousel-home').carousel();
+	
+	$(window).resize(function () {
+		resizeCarouselImage();
+	});
+	
+	resizeCarouselImage();
+	
 });
+
+
+function resizeCarouselImage () {
+	var screenHeight = $(window).height();
+	var topSpace = $("nav").outerHeight();
+	
+	var imgMargin = $("#carousel-home img").outerHeight(true) - $("#carousel-home img").outerHeight();
+	
+	var allowedHeight = screenHeight - topSpace - imgMargin;
+	$('#carousel-home img').height(allowedHeight);
+	$('.carousel-indicators').offset({ top: screenHeight - 10 });
+}
