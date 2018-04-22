770672db9ffbbb1616210ce296b6c6fafb56b1e3
Donglin Pu <p.donglin@gmail.com>
2018-04-22 00:24:53 -0700

    clean up

--- scripts/js/waibao.js
+++ scripts/js/waibao.js
@@ -23,15 +23,32 @@ $(document).ready(function() {
 
 function resizeCarouselImage () {
 	var screenHeight = $(window).height();
+	var screenWidth = $(window).width();
+	
 	var topSpace = $("nav").outerHeight();
 	var imgMargin = $("#carousel-home img").outerHeight(true) - $("#carousel-home img").outerHeight();
 	
 	var allowedHeight = screenHeight - topSpace - imgMargin;
 	
 	$('#carousel-home img').height(allowedHeight);
+	$('#carousel-home img').width("auto");
 	
 	if ($("#section1").hasClass("active")) {
 		$('.carousel-indicators').offset({ top: screenHeight - 10 });
 	}
+	
+	// check width
+	var carouselWidth = $("#carousel-home").width()
+	if (carouselWidth > screenWidth) {
+		$('#carousel-home img').width("100%");
+		$('#carousel-home img').height("auto");
+	}
+	
+	
+//	$('.carousel').carousel();
+//    var deviceHeight = $( window ).height();
+//    var navbarHeight = $(".navbar").height();
+//    var imgHeight = deviceHeight - navbarHeight - 25;
+//    $('.carousel-inner img').css("max-height",imgHeight+"px");
 	
 }
